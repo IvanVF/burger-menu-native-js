491d9a7534b4467064a196474666387e5fe0b36a
IvanVF <fedinivanvladimirovich@mail.ru>
2021-12-06 01:06:45 +0700

    add scroll

--- js/script.js
+++ js/script.js
@@ -43,3 +43,27 @@ if (isMobile.any()) {
     document.body.classList.add("_pc");
 }
 
+
+
+
+const menuLinks  = document.querySelectorAll('.menu__link[data-goto]');
+if (menuLinks.length > 0) {
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener("click", onMenuLinkClick)
+    });
+
+        function onMenuLinkClick(e) {
+            const menuLink = e.target;
+            if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+                const gotoBlock = document.querySelector(menuLink.dataset.goto);
+                const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector("header").offsetHeight;
+
+                window.scroll({
+                    top: gotoBlockValue,
+                    behavior: "smooth"
+                });
+                e.preventDefault();
+            }
+        }
+}
+
