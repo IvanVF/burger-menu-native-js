8543b652dec856dc77ca7a1f84c56c6e7bde609e
IvanVF <fedinivanvladimirovich@mail.ru>
2021-12-06 00:47:29 +0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,45 @@
+"use strict"
+
+const isMobile = {
+    Android: function () {
+        return navigator.userAgent.match(/Android/i)
+    },
+    BlackBerry: function () {
+        return navigator.userAgent.match(/BlackBerry/i)
+    },
+    ios: function () {
+        return navigator.userAgent.match(/iPhoone|ipad|iPod/i)
+    },
+    Opera: function () {
+        return navigator.userAgent.match(/Opera Mini/i)
+    },
+    Windows: function () {
+        return navigator.userAgent.match(/IEMobile/i)
+    },
+    any: function () {
+        return (
+            isMobile.Android() ||
+            isMobile.BlackBerry() ||
+            isMobile.ios() ||
+            isMobile.Opera() ||
+            isMobile.Windows()
+        );
+    }
+};
+
+if (isMobile.any()) {
+    document.body.classList.add("_touch");
+
+    let menuArrows = document.querySelectorAll(".menu__arrow")
+    if (menuArrows.length > 0) {
+        for (let i = 0; i < menuArrows.length; i++) {
+            const menuArrow = menuArrows[i];
+            menuArrow.addEventListener("click", function (e) {
+               menuArrow.parentElement.classList.toggle("_active");
+            });
+        }
+    }
+} else {
+    document.body.classList.add("_pc");
+}
+
